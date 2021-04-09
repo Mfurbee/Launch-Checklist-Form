26dda143534d79957aad5c2b659fee9bfa62482a
Mfurbee <mchristian0428@gmail.com>
2021-04-08 21:39:27 -0500

    Add form validation

--- script.js
+++ script.js
@@ -11,3 +11,34 @@
 </ol>
 <img src="${}">
 */
+
+window.addEventListener("load", function () {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields are required!");
+         return;
+      }
+      else {
+         if (!isNaN(pilotName.value) || !isNaN(copilotName.value) || isNaN(fuelLevel.value) || isNaN(cargoMass.value)){
+            alert("Please check your entries.")
+            return;
+         }
+      }
+
+      let launchStatus = document.getElementById("faultyItems");
+      launchStatus.style.visibility = "visible";
+
+      let pilotStatus = document.querySelector("li[id=pilotStatus]");
+      pilotStatus.innerHTML = `Pilot ${pilotName.value} ready for launch!`;
+
+   });
+
+
+});
+
