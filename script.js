264b8f1c1f735a20c269db2a8e884729aab26691
Mfurbee <mchristian0428@gmail.com>
2021-04-10 19:58:34 -0500

    Finished assignment

--- script.js
+++ script.js
@@ -13,6 +13,26 @@
 */
 
 window.addEventListener("load", function () {
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(json) {
+         const div = document.getElementById("missionTarget");
+         let planet = Math.floor(Math.random() * 6);;
+         // Add HTML that includes the JSON data
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[planet].name}</li>
+            <li>Diameter: ${json[planet].diameter}</li>
+            <li>Star: ${json[planet].star}</li>
+            <li>Distance from Earth: ${json[planet].distance}</li>
+            <li>Number of Moons: ${json[planet].moons}</li>
+         </ol>
+         <img src="${json[planet].image}">
+                  `;
+      });
+   });
+
    let form = document.querySelector("form");
    form.addEventListener("submit", function (event) {
       event.preventDefault();
@@ -31,12 +51,35 @@ window.addEventListener("load", function () {
          }
       }
 
-      let launchStatus = document.getElementById("faultyItems");
-      launchStatus.style.visibility = "visible";
+      let faultyItems = document.getElementById("faultyItems");
+      faultyItems.style.visibility = "visible";
 
       let pilotStatus = document.querySelector("li[id=pilotStatus]");
       pilotStatus.innerHTML = `Pilot ${pilotName.value} ready for launch!`;
+      
+      let copilotStatus = document.querySelector("li[id=copilotStatus]");
+      copilotStatus.innerHTML = `Copilot ${copilotName.value} ready for launch!`;
 
+      let fuelStatus = document.querySelector("li[id=fuelStatus]");
+      let cargoStatus = document.querySelector("li[id=cargoStatus]");
+      let launchStatus = document.getElementById("launchStatus");
+      if (fuelLevel.value < 10000 || cargoMass.value > 10000)
+      {
+         if (fuelLevel.value < 10000)
+            fuelStatus.innerHTML = `Fuel Level ${fuelLevel.value} not enough fuel for the journey!`;
+         if (cargoMass.value > 10000)
+            cargoStatus.innerHTML = `Cargo Mass ${fuelLevel.value} too high for the journey!`;
+         
+         launchStatus.innerHTML = "Shuttle not ready for launch.";
+         launchStatus.style.color = "red";
+      }
+      else 
+      {
+         fuelStatus.innerHTML = `Fuel Level ${fuelLevel.value} ready for launch!`;
+         cargoStatus.innerHTML = `Cargo Mass ${cargoMass.value} ready for launch!`; 
+         launchStatus.innerHTML = "Shuttle is ready for launch!";
+         launchStatus.style.color = "green";
+      }
    });
 
 
